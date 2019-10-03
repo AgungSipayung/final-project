a329151be956d49bbef49ff2774f2c46dabcd06b
AgungSipayung <agungsipayung69@gmail.com>
2019-10-03 14:16:30 +0700

    Membuat Calendar

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,19 +1,40 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
-  
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      plugins: [ 'dayGrid', 'timeGrid', 'list', 'bootstrap' ],
-      timeZone: 'UTC',
-      themeSystem: 'bootstrap',
-      header: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
+  var calendarEl = document.getElementById('calendar');
+
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    plugins: [ 'interaction', 'dayGrid', 'timeGrid', 'list', 'rrule' ],
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
+    },
+    defaultDate: '2018-07-01',
+    editable: true,
+    events: [
+      {
+        title: 'TRIP Surabaya',
+        start: '2018-07-01',
+        className: 'bg-info'
+        
       },
-      weekNumbers: true,
-      eventLimit: true, // allow "more" link when too many events
-      events: 'https://fullcalendar.io/demo-events.json'
-    });
-  
-    calendar.render();
+      {
+          title: 'Long Trip Karimun',
+          start: '2018-07-07',
+          end: '2018-07-10',
+          className: 'bg-danger'
+      },
+      {
+          title: 'Surabaya City Tour',
+          start: '2018-07-12',
+          className: 'bg-dark',
+      },
+    ],
+    eventClick: function(arg) {
+      if (confirm('delete event?')) {
+        arg.event.remove()
+      }
+    }
   });
+
+  calendar.render();
+});
